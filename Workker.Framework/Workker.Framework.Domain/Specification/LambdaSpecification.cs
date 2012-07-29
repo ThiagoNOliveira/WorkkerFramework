93f67359ac5ddf2087ce60ff19595dbc0ef5e101
SammuelGarcia <Sammuel_garcia@hotmail.com>
2012-07-29 19:10:50 -0300

    Specification and Desing by Contract

---
+++ Workker.Framework/Workker.Framework.Domain/Specification/LambdaSpecification.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Workker.Framework.Domain
+{
+    public class LambdaSpecification : CompositeSpecification
+    {
+        private Func<bool> lambda;
+
+        public LambdaSpecification(Func<bool> lambda)
+        {
+            this.lambda = lambda;
+        }
+
+        public override bool IsSatisfied()
+        {
+            return lambda();
+        }
+
+
+    }
+}
